6b6583c295cfdaa36765a96d5b0f2233a443fc98
Erica Lopez <elope213@ucr.edu>
2023-01-06 15:23:20 -0800

    open torrent, get tracker url

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const fs = require('fs');
+const bencode = require('bencode');
+
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+const trackerUrl = torrent.announce.toString('utf8');
